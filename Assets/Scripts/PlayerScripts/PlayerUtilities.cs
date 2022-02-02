c8fa096b5cafb9a3012fd34f74d753047b9d50c7
Nathan N <nhatnguyen2807@gmail.com>
2022-02-02 11:55:27 -0600

    New Vaccines prefab
    Added LnL and Morbina

--- Assets/Scripts/PlayerScripts/PlayerUtilities.cs
+++ Assets/Scripts/PlayerScripts/PlayerUtilities.cs
@@ -19,5 +19,6 @@ public class PlayerUtilities
             player.Stats.Direction = Vector2.zero;
         player.Stats.Position = player.Components.PlayerRidgitBody.position;
         player.Stats.Angle = Mathf.Atan2(player.References.MousePosToPlayer.y, player.References.MousePosToPlayer.x) * Mathf.Rad2Deg;
+        
     }
 }
