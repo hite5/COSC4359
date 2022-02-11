bc8711f655d20ba6f13fcfdb737b21a63c9c5203
dbonill <danbonilla002@gmail.com>
2022-02-11 01:14:52 -0600

    slight changes to wheel

--- Assets/Scripts/UIScript/WheelSectionLogic.cs
+++ Assets/Scripts/UIScript/WheelSectionLogic.cs
@@ -9,32 +9,40 @@ public class WheelSectionLogic : MonoBehaviour
     public bool selected = false;
     public Image WheelPartImage;
     public float wheelPartGrow = 1.5f;
-    public Vector3 growSpeed = new Vector3(0.1f,0.1f,0.1f);
+    public float growSpeed = 0.1f;
+    public Vector3 growSpeedVec;
+    
     public Vector3 DefaultSize; 
     public Vector3 maxSize;
     WaitForSeconds shortWait = new WaitForSeconds(0.1f);
 
-    private void awake()
+
+    void Start()
     {
         DefaultSize = transform.localScale;
         maxSize = DefaultSize * wheelPartGrow;
+        growSpeedVec = new Vector3(growSpeed, growSpeed, growSpeed);
     }
-    
+
 
     // Update is called once per frame
     void Update()
     {
         if (selected == true)
         {
-            WheelPartImage.color = Color.black;
+            //WheelPartImage.color = Color.black;
             if (transform.localScale.x <= maxSize.x)
-                transform.localScale += growSpeed;
+                transform.localScale += growSpeedVec;
+            if (transform.localScale.x > maxSize.x)
+            {
+                transform.localScale = maxSize;
+            }
         }
         else
         {
-            WheelPartImage.color = Color.white;
+            //WheelPartImage.color = Color.white;
             if (transform.localScale.x >= DefaultSize.x)
-                transform.localScale -= growSpeed;
+                transform.localScale -= growSpeedVec;
             if (transform.localScale.x < DefaultSize.x)
             {
                 transform.localScale = DefaultSize;
